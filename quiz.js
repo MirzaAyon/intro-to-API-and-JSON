799ecb4d584a3d100b5e5130738f4ab1adcdfa11
MirzaAyon <ayonmirza98@gmail.com>
2022-06-27 22:03:35 +0600

    quiz added

---
+++ quiz.js
@@ -0,0 +1,17 @@
+1. const stuInfo = JSON.stringify({ name: "James", roll: 3 }); console.log(stuInfo.name);
+ans: undefined
+2.  XHR - xml http request
+3.const premikas = { name: 'keka ferdousi', cars: { brand: 'toyota' } }
+ans: const { brand } = premikas.cars;
+10.
+headers: { 'Content-type': ___ ? ___  },
+What will be “Content - Type ” if you want to send data in JSON format ?
+    ans : application / json
+
+12: What dose HTTP response code 400 refers to ?
+    ans : bad request
+
+11. What status code will you get for a successful API call ?
+    ans : 200
+15. const info = { data: [{ language: "Javascript", library: "React" }] }
+ans: info.data[0].library
